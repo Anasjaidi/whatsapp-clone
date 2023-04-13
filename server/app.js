32a06db6998ab98901b27f8a305a24ef443bdd44
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:15:07 +0000

    retrieve settings and apply some vars to the express app

--- server/app.js
+++ server/app.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const  path  = require('path')
-const dotenv = require('dotenv')
-
-// create instance from express
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
 
 // append .env vars to envirement variables
-dotenv.config('./.env')
+dotenv.config("./.env");
+const env = process.env.NODE_ENV;
 
+// select settings for choosen mode
+const {
+	corsOptions,
+  name,
+	app: { port, debug, logger_format },
+} = require("./config/settings")(env);
+
+// add some vars to express app
+app.set("port", port)
+app.set("debug", debug)
+app.set("env", name)
+
+// create instance from express
+const app = express();
 
 // initialize middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(morgan(process.env.MORGAN_MODE));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './public'), {dotfiles: true}))
+app.use(express.static(path.join(__dirname, "./public"), { dotfiles: true }));
