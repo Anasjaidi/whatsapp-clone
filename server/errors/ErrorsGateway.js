f478b91683381f66e241ea2e007640b90c67b450
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:44:23 +0000

    add test mode errors handler

--- server/errors/ErrorsGateway.js
+++ server/errors/ErrorsGateway.js
@@ -8,6 +8,15 @@ const devErrors = (err, res) => {
 	});
 };
 
+const testErrors = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+		stack: err.stack,
+		err,
+	});
+};
+
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 
