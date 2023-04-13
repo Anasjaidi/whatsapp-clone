39ee22e4a09f2a2235b33cf646c15478df7e6764
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:27:54 +0000

    dependes on http errors package

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const AppError = require('http-errors')
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
@@ -15,13 +16,15 @@ const {
 	app: { port, debug, logger_format },
 } = require("./config/settings")(env);
 
+
+// create instance from express
+const app = express();
+
+
 // add some vars to express app
 app.set("port", port)
 app.set("debug", debug)
 app.set("env", name)
-
-// create instance from express
-const app = express();
 
 // initialize middlewares
 app.use(cors(corsOptions));
@@ -29,3 +32,12 @@ app.use(express.json());
 app.use(morgan(process.env.MORGAN_MODE));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./public"), { dotfiles: true }));
+
+
+// start deafult route
+app.use('*', (req, res, next) => {
+  next(new AppError.NotFound(`resource requested ${req.baseUrl} not found.`))
+})
+
+// start error Gateway
+app.use()
