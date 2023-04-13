978f2ec291f8cb9c58ad1b179ce1d0bbf68dd0e3
anas jaidi <anas.jaidi@icloud.com>
2023-04-13 17:46:26 +0000

    add production mode errors handler

--- server/errors/ErrorsGateway.js
+++ server/errors/ErrorsGateway.js
@@ -1,3 +1,25 @@
+const proErrors = (err, res) => {
+	if (err.isOperational) {
+		// opeartional, trusted: send error to the client
+
+		res.status(err.statusCode).json({
+			status: "fail",
+			message: err.message,
+		});
+
+		// programming or other errors: Don't leak error details
+	} else {
+		// log the error
+		console.log(err);
+
+		// 2) send geniric error
+		res.status(500).json({
+			status: "error",
+			message: "internal server error.",
+		});
+	}
+};
+
 
 const devErrors = (err, res) => {
 	res.status(err.statusCode).json({
