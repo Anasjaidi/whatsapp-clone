d8ae8b44cbf6a83741de4ac071cefd97571d0a6b
Anas jaidi <anas.jaidi@icloud.com>
2023-04-18 14:35:50 +0000

    finish lofin and sign up tabs structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,17 @@ import { Button } from '@chakra-ui/react';
 import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
-
+import './app.css'
 
 
 function App() {
   const [id, setId] = useLocalStorage("id", "")
 
   return (
-		<>
+		<div className='App'>
 			<Route path="/" exact component={Home} />
 			<Route path="/chat" exact component={Chat} />
-		</>
+		</div>
 	);
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,44 @@
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+		<Container maxW={"xl"} centerContent>
+			<Box
+				display="flex"
+				justifyContent={"center"}
+				p={3}
+				bg={"white"}
+				w={"100%"}
+				borderRadius={"lg"}
+				m={"40px 0 15px 0"}
+				borderWidth={"1px"}
+			>
+				<Text fontSize={"4xl"} fontFamily={"work sans"}>
+					Talk-A-Live
+				</Text>
+			</Box>
+			<Box
+				borderWidth={"1px"}
+				p={3}
+				bg={"white"}
+				w={"100%"}
+				borderRadius={"lg"}
+        color={'black'}
+			>
+        <Tabs variant={'soft-rounded'}>
+          <TabList>
+            <Tab>Login</Tab>
+            <Tab>Register</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>login</TabPanel>
+            <TabPanel>register</TabPanel>
+          </TabPanels>
+        </Tabs>
+			</Box>
+		</Container>
+	);
 }
 
 export default Home
